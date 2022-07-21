fdfa73d55f91ca58eb27fa5fcd7d0569b1b953f5
erickbauerv <hullbauer@gmail.com>
2022-07-21 17:54:46 -0300

    adicionado vinculo com api

--- js/script.js
+++ js/script.js
@@ -1,3 +1,29 @@
+const productCardTemplate = document.querySelector("[data-product-template]")
+const productCardContainer = document.querySelector("[data-product-cards-container]")
+const searchInput = document.querySelector("[data-search]")
+
+let products = []
+
+searchInput.addEventListener("input", e => {
+    const value = e.target.value
+    console.log(products)
+})
+
+fetch("https://fakestoreapi.com/products")
+    .then(res => res.json())
+    .then(data => {
+        products = data.map(product => {
+            const card = productCardTemplate.content.cloneNode(true).children[0]
+            const title = card.querySelector("[data-title]")
+            const price = card.querySelector("[data-price]")
+            title.textContent = product.title
+            price.textContent = product.price
+            productCardContainer.append(card)
+            return {title: product.title, price: product.price, element: card}
+        })
+    })
+
+// Menu ---------------------------------------------------------------------------------------
 const navButton = document.getElementById("nav-button");
 
 function toggleMenu(event){
