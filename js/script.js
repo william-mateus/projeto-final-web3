2b0baa62d0f09b0eeeed932c2ffc91320f89abb5
erickbauerv <hullbauer@gmail.com>
2022-07-30 13:32:48 -0300

    adicionado filtragem por categoria

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 const productCardTemplate = document.querySelector("[data-product-template]")
 const productCardContainer = document.querySelector("[data-product-cards-container]")
 const searchInput = document.querySelector("[data-search]")
-const mansClothes = document.querySelector("[data-mans-clothes]")
 
 let products = []
 
@@ -21,14 +20,47 @@ fetch("https://fakestoreapi.com/products")
             const title = card.querySelector("[data-title]")
             const price = card.querySelector("[data-price]")
             const image = card.querySelector("[data-image]")
+            const category = card.querySelector("[data-category]")
             title.textContent = product.title
             price.textContent = product.price
             image.src = product.image
+            category.textContent = product.category
             productCardContainer.append(card)
-            return {title: product.title, price: product.price, inage: product.image, element: card}
+            return {title: product.title, category: product.category, price: product.price, image: product.image, element: card}
         })
     })
 
+function filterProduct(value){
+    let buttons = document.querySelectorAll(".menu-button");
+    buttons.forEach((button) => {
+        if(value.toLowerCase() == button.innerText.toLowerCase()){
+            button.classList.add("button-active");
+        }
+        else{
+            button.classList.remove("button-active")
+        }
+    })
+
+    products.forEach(product => {
+        if(value == "all"){
+            product.element.classList.remove("hidden")
+        } else if(value == "electronics" || value == "jewelery") {
+            const isVisible = product.category.toLowerCase().includes(value.toLowerCase())
+            product.element.classList.toggle('hidden', !isVisible)
+        } else if(value == "mens-clothing" || value == "womens-clothing"){
+            let typeClothing;
+            
+            if(value == "mens-clothing"){
+                typeClothing = "men's clothing"
+            } else {
+                typeClothing = "women's clothing"
+            }
+
+            const isVisible = product.category.toLowerCase().includes(typeClothing.toLowerCase())
+            product.element.classList.toggle('hidden', !isVisible)
+        }
+    })
+}
 // Menu ---------------------------------------------------------------------------------------
 const navButton = document.getElementById("nav-button");
 
